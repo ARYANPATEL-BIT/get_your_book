12d7ea9e096eec1497aee66868c3bd30844e8378
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2026-03-20 00:58:34 +0530

    create post , put, delete endpoints

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -46,3 +46,27 @@ async def read_author_category_by_query(book_author: str, category: str):
            books_to_return.append(book)
 
     return books_to_return
+
+
+@app.post('/books/create_book')
+async def create_book(new_book = Body()):
+    BOOKS.append(new_book)
+
+
+@app.put('/books/update_book')
+async def update_book(update_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == update_book.get('title').casefold():
+            BOOKS[i] = update_book
+
+
+@app.delete('/books/delete_books/{book_title}')
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            return {"message":'Book deleted'}
+     
+    return {'error':'Book not found'}
+
+
