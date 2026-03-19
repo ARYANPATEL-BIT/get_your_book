cfe376930365886dc60fc2a12f946927c5f2e31d
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2026-03-20 00:20:05 +0530

    created some endpoints

--- books.py
+++ books.py
@@ -18,8 +18,31 @@ async def read_all_books():
     return BOOKS
 
 
-@app.get('/books/{dynamic_param}')
-async def read_all_books(dynamic_param: str):
-    return {'dynamic_param': dynamic_param}
+@app.get('/books/{book_title}')
+async def read_all_books(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+@app.get('/books/')
+async def read_category_by_query(category: str):
+    books_to_return = []
+
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    
+    return books_to_return
 
 
+@app.get('/books/{book_author}')
+async def read_author_category_by_query(book_author: str, category: str):
+
+    books_to_return = []
+
+    for book in BOOKS:
+        if book.get('book_author').casefold() == book_author.casefold() and \
+           book.get('category').casefold() == category.casefold():
+           books_to_return.append(book)
+
+    return books_to_return
